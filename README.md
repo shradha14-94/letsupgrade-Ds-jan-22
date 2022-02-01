fcd9e1283de81d9179f2fb2b5c3e16a507993231
Shradha Amit Gilbile <70842146+shradha14-94@users.noreply.github.com>
2022-02-01 20:32:49 +0530

    Added EvenOdd  function

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # letsupgrade-Ds-jan-22
 assignment repository
+
+num = int(input("Enter a number : ))
+if(num%2) == 0:
+    print("{0} is Even number".format(num))
+else:
+     print("{0} is Odd Number".format(num))
---
+++ evenoddnum.py
@@ -0,0 +1,8 @@
+# python program to check given number is odd or even
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+    print("{0} is Even".format(num))
+else:
+    print("{0} is Odd".format(num))
+        
